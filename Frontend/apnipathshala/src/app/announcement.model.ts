2e07be21f3970c621ac0f88643202275485ff5c1
aman1722 <112754413+aman1722@users.noreply.github.com>
2023-09-06 00:57:48 +0530

    node backend

--- Frontend/apnipathshala/src/app/announcement.model.ts
+++
@@ -1,9 +0,0 @@
-
-export interface Announcement {
-    id: number;
-    title: string;
-    description: string;
-    publish_date: Date;
-    department: number;
-    course: number;
-  }
--- Frontend/apnipathshala/src/app/submission.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Submission {
-    id?: number; 
-    submission_date: string;
-    status: string; 
-    remarks: string;
-    assignment: number; 
-    student: number; 
-  }
